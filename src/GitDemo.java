30bc3db4a0f79c5491c61c9833c7d63f271f3976
Vibhuti <vibhutiparmar19@gmail.com>
2023-04-26 13:36:26 -0600

    New Commit

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -4,6 +4,10 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("We have made new file in this project.");
+			System.out.println("This is new commit");
+			System.out.println("This is new commit");
+			System.out.println("This is new commit");
+			System.out.println("This is new commit");
 	}
 
 }
